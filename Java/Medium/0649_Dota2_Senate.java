bca66c3266654d21861cfe701d083eb0a9b06e06
Dhaka08 <himanshudhaka05@gmail.com>
2026-01-30 20:48:18 +0530

    Leetcode Medium: Dota2 Senate

---
+++ Java/Medium/0649_Dota2_Senate.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> radiant = new LinkedList<>();
+        Queue<Integer> dire = new LinkedList<>();
+        int n = senate.length();
+        for(int i =0;i<n;i++){
+            if(senate.charAt(i)=='R'){
+                radiant.add(i);
+            }
+            else{
+                dire.add(i);
+            }
+        }
+        while(!radiant.isEmpty() && !dire.isEmpty()){
+            if(radiant.peek()<dire.peek()){
+                radiant.add(n++);
+            }
+            else{
+                dire.add(n++);
+            }
+            radiant.poll();
+            dire.poll();
+        }
+        if(radiant.size()==0){
+            return "Dire";
+        }
+        else{
+            return "Radiant";
+        }        
+    }
+}
