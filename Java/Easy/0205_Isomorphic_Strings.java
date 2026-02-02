94b7ec31d39a412d6063212d57cf4e553d145295
Dhaka08 <himanshudhaka05@gmail.com>
2026-02-02 19:44:38 +0530

    LeetCode Easy: Isomorphic Strings

---
+++ Java/Easy/0205_Isomorphic_Strings.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> mp = new HashMap<>();
+        for(int i =0;i<s.length();i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if(mp.containsKey(ch1)){
+                if(mp.get(ch1)!=ch2) return false;
+            }
+            else if(mp.containsValue(ch2)){
+                return false;
+            }
+            else{
+                mp.put(ch1,ch2);
+            }
+        }
+        return true;
+    }
+}
