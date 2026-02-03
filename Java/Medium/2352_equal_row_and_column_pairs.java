70b02c483760b721f6721b5846ba9cde707cad39
Dhaka08 <himanshudhaka05@gmail.com>
2026-02-03 16:29:34 +0530

    LeetCode Medium

---
+++ Java/Medium/2352_equal_row_and_column_pairs.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        int n = grid.length;
+        HashMap<String,Integer> mp = new HashMap<>();
+        //store rows
+        for(int i =0;i<n;i++){
+            String key = Arrays.toString(grid[i]);
+            if(mp.containsKey(key)){
+                mp.put(key,mp.get(key)+1);
+            }
+            else{
+                mp.put(key,1);
+            }
+        }
+        int count =0;
+        //check columns
+        for(int j =0;j<n;j++){
+            int [] col = new int[n];
+            for(int i =0;i<n;i++){
+                col[i]=grid[i][j];
+            }
+            String key = Arrays.toString(col);
+            if(mp.containsKey(key)){
+                count+=mp.get(key);
+            }
+            else{
+                count+=0;
+            }
+        }
+        return count;
+    }
+}
