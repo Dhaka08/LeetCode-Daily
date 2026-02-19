8e9a0f9c705bc2dbbb937f430d4b65db6f0a2cb1
Dhaka08 <himanshudhaka05@gmail.com>
2026-02-19 09:41:39 +0530

    Leetcode Medium

---
+++ Java/Medium/0046_Permutations.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public void bactrack(List<List<Integer>> result,int []nums,int start){
+        if(start==nums.length){
+            result.add(toList(nums));
+        }
+        else{
+            for(int i = start;i<nums.length;i++){
+                swap(i,start,nums);
+                bactrack(result,nums,start+1);
+                swap(i,start,nums);
+            }
+        }
+    }
+    public List<Integer> toList(int[] nums){
+        List<Integer> res = new ArrayList<>();
+        for(int i: nums){
+            res.add(i);
+        }
+        return res;
+    }
+    public void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i]= nums[j];
+        nums[j]= temp;
+    }
+
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        bactrack(result,nums,0);
+        return result;
+    }
+}
