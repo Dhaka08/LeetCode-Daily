0c5c92a1f5e00e0c5256c7146d3ede4c1cb27f12
Dhaka08 <himanshudhaka05@gmail.com>
2026-03-20 18:09:58 +0530

    Leetcode Medium

---
+++ Java/Medium/0450_Delete_Node_in_a_BST.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode findpredecessor(TreeNode node){
+        while(node.right!=null){
+            node = node.right;
+        }
+        return node;
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return root;
+        if(key<root.val) root.left = deleteNode(root.left,key);
+        else if(key>root.val) root.right = deleteNode(root.right,key);
+        else{ //root.val = key
+        if(root.left==null && root.right==null) return null;
+        if(root.left==null) return root.right;
+        else if (root.right==null) return root.left;
+        else{ // 2child
+        TreeNode node = findpredecessor(root.left);
+        root.val = node.val;
+        root.left = deleteNode(root.left,node.val);
+        }
+    }
+        return root;
+  }
+}
