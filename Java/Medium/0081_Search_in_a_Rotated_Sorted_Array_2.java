848e253ba1110bbcd12287f9833c7950527a2a5a
Dhaka08 <himanshudhaka05@gmail.com>
2026-04-03 17:59:34 +0530

    Leetcode Medium

---
+++ Java/Medium/0081_Search_in_a_Rotated_Sorted_Array_2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int st = 0, end = n-1;
+        while(st<=end){
+            int mid = st + (end-st)/2;
+            if(nums[mid]==target) return true;
+            else if(nums[st]==nums[mid] && nums[end]== nums[mid]){
+                st++;
+                end--;
+            }
+            else if(nums[mid]<=nums[end]){// mid to end is sorted
+                if(target > nums[mid] && target<=nums[end]){
+                    st = mid + 1;
+                }
+                else{
+                    end = mid -1;
+                }
+            }
+            else{
+                if(target>=nums[st] && target<nums[mid]){
+                    end = mid -1;
+                }
+                else{
+                    st =  mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+}
